4aeb226d4702bc15e5c4ab8712dfad0f27e29784
Koala06 <barna.geller@gmail.com>
2022-06-20 10:55:53 +0200

    initial project commit

---
+++ Paket_4157/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Paket_4157
+{
+    class SampleClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+        }
+    }
+}
